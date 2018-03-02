26c0219f3c4c3d93d61c14a31e2bf70f74877f3e
sinclairj <sunil.jadhav2289@gmail.com>
2018-03-03 04:31:49 +0530

    update script

--- launch_container.sh
+++ launch_container.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-echo "hello world"
+docker stop myapche
+docker rename myapche "myapche_`date +"%d-%m-%Y"`"
+docker pull 192.168.56.160:5000/myimage:latest
+docker run -d -t --name myapche -p 2222:22 -p 80:80 192.168.56.160:5000/myimage:latest
+
+
