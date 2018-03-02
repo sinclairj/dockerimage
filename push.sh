0e8e9e73d51d566a39f241198c25b62a8b671d4a
sinclairj <sunil.jadhav2289@gmail.com>
2018-03-03 03:38:16 +0530

    upated file

--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker tag myimage:latest 192.168.56.160:5000/myimage:latest
-docker push 192.168.56.160:5000/myimage:latest
+sudo docker tag myimage:latest 192.168.56.160:5000/myimage:latest
+sudo docker push 192.168.56.160:5000/myimage:latest
