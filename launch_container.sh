c4761b205c3a37b46639149ee659e9a8f0967ea1
sinclairj <sunil.jadhav2289@gmail.com>
2018-03-03 07:55:10 +0530

    dockerfile update

--- launch_container.sh
+++ launch_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker stop myapche
-docker rename myapche "myapche_`date +"%H%M%-%d-%m-%Y"`"
+docker rename myapche "myapche_`date +"%H%M-%d-%m-%Y"`"
 docker pull 192.168.56.160:5000/myimage:latest
 docker run -d -t --name myapche -p 2222:22 -p 80:80 192.168.56.160:5000/myimage:latest
 
